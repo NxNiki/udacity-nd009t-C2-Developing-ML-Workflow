c2ead87900d00e37a3911d440174eb47de8f495c
EC2 Default User <ec2-user@ip-172-16-41-195.ec2.internal>
2025-03-28 00:03:03 +0000

    lesson 2 exercise 5 and lesson 3 exercise 1 (wip)

--- lesson3/HelloBlazePreprocessLambda.py
+++ lesson3/HelloBlazePreprocessLambda.py
@@ -5,8 +5,8 @@ import os
 import boto3
 from botocore.exceptions import ClientError
 
-BUCKET_NAME = 'FILL_THIS_IN'
-PREFIX = 'FILL_THIS_IN'
+BUCKET_NAME = 'udacity-xin-mlworkflow'
+PREFIX = 'lession3-exercise1'
 
 # Function below unzips the archive to the local directory. 
 
